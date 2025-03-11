7a47031dbe020f4cf2672152bfedf4ba93c157b2
JoaoMatheus27 <165078857+JoaoMatheus27@users.noreply.github.com>
2025-03-11 14:33:55 +0000

    fazer tela de login

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import Bg from "../../public/img/bg.png";
+import lupa from "../../public/img/lupa.png";
 import BUTTONAPP from "../components/BUTTONAPP/button.jsx";
 
 export default function Home() {
   return (
     <main className="h-[100%]">
-      <section className="h-[53svh]   bg-[url('/img/bg.png')]  bg-black opacity-90 bg-cover   bg-no-repeat sm:bg-cover sm:bg-no-repeat sm:bg-center md:bg-cover md:bg-center-center">
-        <p className="font-julius text-white font-thin text-3xl text-900  flex items-center justify-center h-[5svh] px-5 ">
+      <section className="flex items-center h-[53svh]   bg-[url('/img/bg.png')]  bg-black opacity-90 bg-cover   bg-no-repeat sm:bg-cover sm:bg-no-repeat sm:bg-center md:bg-cover md:bg-center-center">
+        <p className=" font-julius text-white font-thin text-3xl h-[15svh] px-5 ">
           Compare seus produtos e compre barato e com confiança
         </p>
       </section>
@@ -16,12 +17,14 @@ export default function Home() {
           Pesquise produtos e os compare!
         </p>
         {/* input */}
-        <div className="relative w-full flex justify-center space-x-5 flex-wrap p-5">
+        <div className="relative w-full flex m-0 justify-center space-x-5 flex-wrap p-5">
           <input
             className=" font-inter px-5 rounded-full w-[30svh] h-[6svh] outline-none opacity-50 "
             placeholder="Pesquise aqui..."
           ></input>
-          <i className="fas fa-search fixed left-[32svh] top-[68.7%] opacity-50 text-black"></i>
+          <div className="relative right-14 top-3">
+            <Image src={lupa} className="fixed opacity-50 text-black w-5 h-5"></Image>
+          </div>
         </div>
         {/* botao pesquisar */}
         <div className="w-full flex justify-center space-x-5 flex-wrap p-5">
