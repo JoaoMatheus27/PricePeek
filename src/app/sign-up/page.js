2b146519a91adf4f5a5c7543829862cc7a8c64ef
JoaoMatheus27 <165078857+JoaoMatheus27@users.noreply.github.com>
2025-03-13 16:00:37 -0300

    #

--- src/app/sign-up/page.js
+++ src/app/sign-up/page.js
@@ -1,14 +1,13 @@
+import InputLogin from "@/components/layouts/INPUT-CONTAINER/InputLogin";
 import NavAuth from "@/components/layouts/NAV/NavAuth";
 
 export default function Signup() {
   return (
-    <div className="h-[100vh]">
+    <div className=" flex h-[100vh] bg-vermelho text-center items-start grid flex-row">
       <NavAuth />
-      <main className=" bg-vermelho">
+      <main className="bg-[white] rounded-2xl h-[70%]">
         <h1>Cadastre-se</h1>
-        <section>
-          
-        </section>
+        <InputLogin />
       </main>
     </div>
   );
--- src/components/layouts/INPUT-CONTAINER/InputLogin.jsx
+++ src/components/layouts/INPUT-CONTAINER/InputLogin.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
 
 function InputLogin() {
   return (
-    <div>
-        input
+    <div className="flex justify-center items-center h-[20vh] bg-gray-100">
+      <div className="flex flex-col gap-6 w-[90%] max-w-md p-6 bg-white rounded-xl shadow-lg">
+        <input
+          className="border border-black rounded-xl p-2 h-[6vh]"
+          type="text"
+          placeholder="Insira seu nome"
+        />
+        <input
+          className="border border-black rounded-xl p-2 h-[6vh]"
+          type="email"
+          placeholder="Insira seu email"
+        />
+        <input
+          className="border border-black rounded-xl p-2 h-[6vh]"
+          type="password"
+          placeholder="Insira sua senha"
+        />
+        <input
+          className="border border-black rounded-xl p-2 h-[6vh]"
+          type="password"
+          placeholder="Confirme sua senha"
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default InputLogin
+export default InputLogin;
