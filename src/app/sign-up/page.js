bcc7dfbeffe1bbebd10b406c8862de08938de549
JoaoMatheus27 <165078857+JoaoMatheus27@users.noreply.github.com>
2025-04-03 14:35:47 +0000

    #

--- src/app/sign-up/page.js
+++ src/app/sign-up/page.js
@@ -1,44 +1,17 @@
-<<<<<<< HEAD
 import ButtonSignup from "@/components/BUTTONAPP/ButtonSignup";
 import InputSignup from "@/components/layouts/INPUT-CONTAINER/InputSignup";
-=======
-<<<<<<< HEAD
-import ButtonSignup from "@/components/BUTTONAPP/ButtonSignup";
-import InputSignup from "@/components/layouts/INPUT-CONTAINER/InputSignup";
-=======
-import InputLogin from "@/components/layouts/INPUT-CONTAINER/InputLogin";
->>>>>>> origin/main
->>>>>>> refs/remotes/origin/main
 import NavAuth from "@/components/layouts/NAV/NavAuth";
 
 export default function Signup() {
   return (
-<<<<<<< HEAD
-    <div className="flex flex-col items-center h-[100svh]">
-      <NavAuth />
-      <main className=" h-[100%] w-[100%] bg-vermelho">
-        <section className="flex items-center justify-center mt-[25%]">
-          <InputSignup />
-        </section>
-=======
-<<<<<<< HEAD
-    <div className="flex flex-col items-center h-[100svh]">
-      <NavAuth />
-      <main className=" h-[100%] w-[100%] bg-vermelho">
-        <section className="flex items-center justify-center mt-[25%]">
-          <InputSignup />
-        </section>
-=======
-    <div className=" flex h-[100vh] bg-vermelho text-center items-start grid flex-row">
-      <NavAuth />
-      <main className="bg-[white] rounded-2xl h-[70%]">
-        <h1>Cadastre-se</h1>
-        <InputLogin />
->>>>>>> origin/main
->>>>>>> refs/remotes/origin/main
-      </main>
-    </div>
-  );
+      <div className="flex flex-col items-center h-[100svh]">
+        <NavAuth />
+        <main className=" h-[100%] w-[100%] bg-vermelho">
+          <section className="flex items-center justify-center mt-[25%]">
+            <InputSignup />
+          </section>
+        </main>  
+      </div>
+  )
 }
 
-// uiverse.io
